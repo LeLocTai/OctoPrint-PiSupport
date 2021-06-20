fb945b971b47f87f34275f1df25b353a6bb73899
Tai Le <contact@leloctai.com>
2021-06-20 17:46:46 +0700

    Revert "change ids"
    This reverts commit d9f459114a522122aae7a6cab6d8f110e90ae8e2.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "pi_support_custom"
+plugin_identifier = "pi_support"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_pi_support_custom"
+plugin_package = "octoprint_pi_support"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-PiSupport-Custom"
+plugin_name = "OctoPrint-PiSupport"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "2021.6.14"
